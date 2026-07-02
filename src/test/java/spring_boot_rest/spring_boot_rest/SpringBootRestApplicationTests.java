63f90bf976eb3079a742f56fb33959b6cd41d604
WinHome Test <144950366+Randomlyclueless@users.noreply.github.com>
2026-07-02 16:26:34 +0530

    Initial Spring Boot REST project

---
+++ src/test/java/spring_boot_rest/spring_boot_rest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_rest.spring_boot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
